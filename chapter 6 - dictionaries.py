f726bd64d433fec48a980d19bf4793083046f364
Max Misaro <kossoff124@gmail.com>
2026-05-11 14:44:17 +0300

    Played around Dictionaries and will working with Nesting next

--- chapter 6 - dictionaries.py
+++ chapter 6 - dictionaries.py
@@ -31,3 +31,54 @@ del cat['speed']
 print(cat)
 cat['speed'] = 'fast'
 print(cat)
+
+#practicing get()
+alien_1 = {'color': 'red', 'speed': 'ultra'}#,'points':88}
+#good example - if we dont have point key in the dict it will text us No point values assigned
+#I suppouse command get checks dictionary on key pairs and insted of error in this case 
+#I can recieve custom message
+#also I can simply use get('key-value-name') - and if its not exist I will recieve 'None'
+point_value = alien_1.get('points') #, 'No point values assigned')
+print(point_value)
+
+#practicing for loop in dictionary
+cat_2 = {
+    'chanel':'sayamese',
+    'boris':'pers',
+    'jinx':'japanese',
+    'yamamoto':'japanese'
+}
+for k, v in cat_2.items(): #items() iterates all the key-values in dictionaries
+    print(f"\nKey: {k}")
+    print(f"Value:{v}")
+# if I need only keys I can use keys() command or values() for value
+for nick in cat_2.keys():
+    print(f"\nKey:{nick}")
+#and can not to use keys()
+for nick in cat_2:
+    print(f"\nKey:{nick}")
+#to receive values in sorted from I can use sorted() function
+for nick in sorted(cat_2):
+    print(f"\nKey:{nick.upper()}")
+#playing with values()
+#book says that values could be repetative and to make us recieve unique
+# value we can utilise set()
+for nick in set(cat_2.values()):
+    print(f"\nKey:{nick.lower()}")
+
+#tough exercise but I nailed it
+#I got confused about how my string will understand thta I want languages
+#but I assume I assigned name as name than it check using name as key to find its value pair in fav_languages
+fav_languages = {
+    'Emma':'Python',
+    'John':'c#'
+}
+poll_invitation = ['Emma','John','Patrick','Max']
+for name in poll_invitation:
+    if name in fav_languages:
+        language = fav_languages[name].title()
+        print(f"Thank you {name} for participation and choosing {language} ")
+    else:
+        print(f"Dear {name} its a reminder to take part in our poll")
+
+###NESTING
---
+++ gitcommands.md
@@ -0,0 +1,8 @@
+git add .
+git commit -m "chapter 7 exercises done"
+git push
+
+On another PC:
+git clone https://github.com/MaxWell1ams/python_work.git
+And then each time I sit down on that PC:
+git pull   # get latest changes before starting
