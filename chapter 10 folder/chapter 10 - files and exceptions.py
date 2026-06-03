3098bd89f22ddbf4ee970aa8aa44db0bbf3dc075
Max Misaro <kossoff124@gmail.com>
2026-06-04 00:04:23 +0300

    practicing files, exceptions

--- chapter 10 folder/chapter 10 - files and exceptions.py
+++ chapter 10 folder/chapter 10 - files and exceptions.py
@@ -20,7 +20,7 @@ content = "Python becoming my first programming language\n"
 content += "I'm interested in C++\n"
 content += "I know that I need also learn JS\n"
 path = Path('chapter 10 folder/txtfile_write.txt')
-path.write_text(content) 
+path.write_text(content)
 #also I discovered that write_text - erasing what file had and overwrites data
 
 #at least I can do program which will store(but overwrite) and read data in a file
@@ -34,15 +34,58 @@ while active:
     login = input("login:\n")
     password = input("password:\n")
     print("loggining....")
-    if password in lines:
+    if password.lower() in lines:
         print("welcome back")
     else:
         print("oh you new one bro")
-        path.write_text(f"{password}\n") #its cant write login,password in one run
+        #path.write_text(f"{password}\n") --- #its cant write login,password in one run
         #so I can use only 1 value and its will be pass 
         # cuz next value will overwrite it so will use pass
+        with open(path, 'a') as f: #I researched that I can utilise a - as append
+            # w as write and r as read in format above
+            f.write(f"{login} \n")
+            f.write(f"{password} \n")
     quit = input("wanna quit or relogin?(quit/relogin)")
     if quit == 'quit':
         active = False
 
+
 ### Exeptions - so its basically try, except blocks to avoid errors
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("I cann't divide by 0")
+
+### divider with exceptions - very good sample of how to utilise it
+while True:
+    st_nuumber = input("first number: \n")
+    if st_nuumber == 'q':
+        break
+    second_number = input("second number: \n")
+    if second_number == 'q':
+        break
+    try: #here block where I trying answer but with exception
+        #and the only code whih should go to try is one which likely
+        #will cause error
+        answer = int(st_nuumber) / int(second_number)
+    except ZeroDivisionError:
+        print("you are cringe!")
+    except ValueError:  #trying additional except - works well
+        print("you are super cringe!")
+    else:
+        print(answer)
+
+#and so on for example for FileNotFoundError
+from pathlib import Path
+path = Path('sdds.txt')
+try:
+    contents = path.read_text(encoding='utf-8')
+except FileNotFoundError:
+    print(f"yo I don't see {path}")
+
+#in same way I use except I can use pass under it to not 
+# show notification
+except FileNotFoundError:
+    pass
+
+
