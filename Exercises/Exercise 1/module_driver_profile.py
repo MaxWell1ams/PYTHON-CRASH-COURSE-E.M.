23c165b6d618f872f6af523cf58076433b049058
Max Misaro <kossoff124@gmail.com>
2026-06-10 18:48:56 +0300

    Practicing to memorise what i learned

---
+++ Exercises/Exercise 1/module_driver_profile.py
@@ -0,0 +1,11 @@
+class DriverProfile:
+    """Describes simracer-driver profile"""
+    def __init__ (self,name, primary_game, wheel_hardware):
+        self.name = name
+        self.game = primary_game
+        self.wheel = wheel_hardware
+
+    def record_lap_time(self, track, **lap_time):
+        """Stores these lap times in a dictionary attribute"""
+        self.lap_time = lap_time
+        self.track = track
