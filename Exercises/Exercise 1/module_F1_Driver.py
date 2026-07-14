20a9d4f944939a84a12aa47519c4e03ed7216369
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-07-15 02:39:43 +0300

    tried to solve smth, asked ai to teach but got a lot of solved problems, will restart tomorrow from scratch

--- Exercises/Exercise 1/module_F1_Driver.py
+++ Exercises/Exercise 1/module_F1_Driver.py
@@ -1,22 +1,22 @@
 from module_driver_profile import DriverProfile
 class F1Driver(DriverProfile):
-    def __init__ (self, ers, name, primary_game, wheel_hardware):
+    def __init__ (self, name, primary_game, wheel_hardware):
         """ERS mode data"""
         super().__init__(name,primary_game,wheel_hardware)
-        self.ers = 400
+        self.ers_lvl = 400
     def ers(self, capacity):
         """capacity of ERS"""
         self.capacity = capacity
-        print(f"ERS capacity of your F1 car {self.ers}kw")
+        print(f"ERS capacity of your F1 car {self.ers_lvl}kw")
 
     def ers_update (self, kw):
         """Update ERS mode data"""
         if kw >= self.capacity:
-            self.ers = kw
+            self.ers_lvl = kw
         else:
             print("FIA might ban your ERS (out of regulations)")
     def increase_ers(self, kw):
-        self.ers += kw
+        self.ers_lvl += kw
 
     def decrease_ers(self, kw):
-        self.ers -= kw
+        self.ers_lvl -= kw
